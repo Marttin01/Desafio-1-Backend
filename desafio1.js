421d5398cff4d5bebc1f5960a8663f38b2569727
Marttin01 <martin.xd@hotmail.com.ar>
2023-02-16 23:13:08 -0300

    desafio1

---
+++ desafio1.js
@@ -0,0 +1,16 @@
+// class ProductManager {
+//     constructor () {
+//         this.products = [];
+//     }
+
+//     addProduct(product) {
+//         this.products.push(product)
+//     }
+// }
+
+// const storage = new ProductManager
+
+// storage.addProduct({title:"silla", code: 01})
+// console.log(storage)
+
+console.log("hola mundo")
